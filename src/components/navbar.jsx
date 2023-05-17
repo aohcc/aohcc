dfa879489481f6b8e6848c7352db48dfdda7e901
Ryan O'Farrell <ryanof@pm.me>
2023-05-17 10:39:03 -0400

    Developed navbar using Bootstrap.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,23 @@
-import { NavLink, Link, Outlet } from 'react-router-dom'
 import '../index.css'
 
 export default function Navbar() {
   return (
     <>
       <nav className="navbar navbar-light bg-dark flex-column flex-md-row">
-        <NavLink className="navbar-brand ml-1" to="/">
+        <a className="navbar-brand ml-1" href="#">
           <img className="mr-2 rounded" src={require('../AOH-Logo_White_transparent.png')} width="100px" height="70px" />
-        </NavLink>
+        </a>
         <div>
           <ul className="flex-row navbar-nav">
-            <NavLink className="btn btn-outline-light mr-4" to="/projects">Projects</NavLink>
-            <NavLink className="btn btn-outline-light mr-1" to="/contact-me">Contact me</NavLink>
+            <li className="nav-item active">
+              <a className="btn btn-outline-light mr-4" href="#about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="btn btn-outline-light mr-4" href="#therapists">Therapists</a>
+            </li>
+            <li className="nav-item">
+              <a className="btn btn-outline-light mr-1" href="#contact">Contact</a>
+            </li>
           </ul>
         </div>
       </nav>

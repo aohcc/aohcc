363db1e4e51159b9b881d11a9ef710a92ccf6cbd
Ryan O'Farrell <ryanof@pm.me>
2023-05-17 16:37:28 -0400

    Added collapsable bios to cards.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <About />
-      <div className="d-flex justify-content-center mb-5" id="therapists">
-        <Therapist />
-      </div>
+      <Therapist />
     </div>
   );
 }
--- src/components/therapists.jsx
+++ src/components/therapists.jsx
@@ -1,11 +1,47 @@
 export default function Therapist() {
-  return (
-    <div className="card col-md-4">
-      <div className="card-body">
-        <h5 className="card-title">Dr. Ryan O'Farrell, Psy.D.</h5>
-        <h6 className="card-subtitle">Co-owner and Psychologist</h6>
-        <p className="card-text">Placeholder</p>
-      </div>
+  const therapists = [
+    {
+      id: "ryan",
+      name: "Dr. Ryan O'Farrell, Psy.D.",
+      firstName: "Ryan",
+      title: "Co-owner and Psychologist",
+      bio: "Placeholder"
+    },
+    {
+      id: "jacquie",
+      name: "Jacquie Atkins, MS, LPC",
+      firstName: "Jacquie",
+      title: "Co-owner and Licensed Professional Counselor",
+      bio: "Placeholder"
+    }
+  ]
+
+  const therapistCard = therapists.map(therapist =>
+    <div>
+        <div className="card mx-auto col-8 mb-3">
+            <div className="card-body">
+                <h5 className="card-title">{therapist.name}</h5>
+                <h6 className="card-subtitle">{therapist.title}</h6>
+                <p className="card-text">{therapist.bio}</p>
+            </div>
+        </div>
+        <button className="btn btn-primary mb-3" type="button" data-bs-toggle="collapse" data-bs-target={"#" + therapist.id} aria-expanded="false" aria-controls={therapist.id}>
+          {therapist.firstName}'s bio
+        </button>
+        <div className="collapse mx-auto col-8" id={therapist.id}>
+            <div className="card card-body">
+                Placeholder
+            </div>
+        </div>
     </div>
+  )
+
+  return (
+    <>
+        <h2 className="card mx-auto col-md-4 p-2 mb-5">Therapists</h2>
+        <div className="row-cols-2 d-flex justify-content-around mb-5" id="therapists">
+            {therapistCard}
+        </div>
+    </>
   )
 }

05d2066dea4175c3acddd89cf6c7666d5169ef74
Ryan O'Farrell <ryanof@pm.me>
2023-06-26 18:21:20 -0400

    Attempted to add footer and updated package.json for deploying on Github Pages.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/about'
 import Therapist from './components/therapists'
 import Faq from './components/faq'
 import Contact from './components/contact'
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Therapist />
       <Faq />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  <>
+    <footer className="d-flex justify-content-center bg-dark">
+        <p className="text-center text-light mt-3">928 Savage Rd.<br/>Charleston, SC 29414<br/>Phone: (843) 377-5322</p>
+    </footer>
+  </>
+}

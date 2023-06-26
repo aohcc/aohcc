2107e2965bdee4ba658afe3c44580ccdb0f22a69
Ryan O'Farrell <ryanof@pm.me>
2023-06-26 18:35:24 -0400

    Still trying to deploy.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   <>
-    <footer className="d-flex justify-content-center bg-dark">
+    <footer className="d-flex justify-content-center bg-dark p-3">
         <p className="text-center text-light mt-3">928 Savage Rd.<br/>Charleston, SC 29414<br/>Phone: (843) 377-5322</p>
     </footer>
   </>

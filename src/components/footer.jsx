1dde5ba4f3729186c0de959eeccde006dd0c34fa
Ryan O'Farrell <ryanof@pm.me>
2023-07-08 14:12:30 -0400

    Changed photo links to open in new tab.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="footer-color d-flex flex-column justify-content-center p-3">
         <p className="text-center text-light mt-3">928 Savage Rd.<br/>Charleston, SC 29414<br/>Phone: (843) 377-5322</p>
         <p><a href={npp} target="_blank">AOHCC Notice of Privacy Practices</a></p>
-        <p className="text-center text-light">Photo by <a href="https://unsplash.com/@pioneermedia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Leighton Smith</a> on <a href="https://unsplash.com/photos/NPxkSK-makg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+        <p className="text-center text-light">Photo by <a href="https://unsplash.com/@pioneermedia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Leighton Smith</a> on <a href="https://unsplash.com/photos/NPxkSK-makg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" target="_blank">Unsplash</a>
   </p>
     </footer>
   </>
